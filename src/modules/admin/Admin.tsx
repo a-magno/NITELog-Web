a7d389771c2bc247e25afec0fa8dfc96df074eb1
Zokyn <gustavo.casaes@mail.uft.edu.br>
2025-08-08 11:26:44 -0300

    feat(admin-panel): adicionado abas ao container da tabela

--- src/modules/admin/Admin.tsx
+++ src/modules/admin/Admin.tsx
@@ -13,12 +13,22 @@ const Admin = () => {
             <button className="searchButton">
               <i className="searchIcon">Q</i>
             </button>
-            <input type="search" className="searchInput" />
+            <input
+              type="search"
+              placeholder="Buscar por usuários"
+              className="searchInput"
+            />
             <button className="searchButton">
               <span>Pesquisar</span>
             </button>
           </div>
-          <div className="table-container"></div>
+          <div className="table-container">
+            <ul className="tabs-container">
+              <li className="tab-option active">Usuários</li>
+              <li className="tab-option">Reuniões</li>
+              <li className="tab-option">Projetos</li>
+            </ul>
+          </div>
         </div>
       </main>
     </>
