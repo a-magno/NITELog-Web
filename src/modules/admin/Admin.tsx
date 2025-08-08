bbecd18d711d004becc2ab742ea6517b2e239e4a
Zokyn <gustavo.casaes@mail.uft.edu.br>
2025-08-08 10:49:41 -0300

    feat(admin-panel): adicionado componentes básico para a barra de busca

--- src/modules/admin/Admin.tsx
+++ src/modules/admin/Admin.tsx
@@ -8,11 +8,18 @@ const Admin = () => {
         <NavBar />
       </header>
       <main id="admin">
-        <div className="searchbar">
-          <i className="searchIcon">Q</i>
-          <input type="search" name="searchInput" id="searchInput" />
+        <div>
+          <div className="searchbar">
+            <button className="searchButton">
+              <i className="searchIcon">Q</i>
+            </button>
+            <input type="search" className="searchInput" />
+            <button className="searchButton">
+              <span>Pesquisar</span>
+            </button>
+          </div>
+          <div className="table-container"></div>
         </div>
-        <div className="table-container"></div>
       </main>
     </>
   );
