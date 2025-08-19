e86eed40e88d2c352c21326c463f9ff0e243579e
Zokyn <gustavo.casaes@mail.uft.edu.br>
2025-08-19 17:57:33 -0300

    feat(user-page): adicionado informações de contato do usuário e adicionado estrutura do cabeçalho do conteudo principal

--- src/modules/user/User.tsx
+++ src/modules/user/User.tsx
@@ -5,8 +5,13 @@ import "@styles/user.css";
 import logo from "@images/nite-logo.png";
 
 const User = () => {
-  const [name] = useState("Beltrano");
+  const [fname] = useState("Beltrano");
+  const [mname] = useState("Silva");
+  const [lname] = useState("Alves");
+  const [username] = useState("Chad Belt");
   const [email] = useState("beltrano@gmail.com");
+  const [linkedin] = useState("Beltrano Silva");
+  const [github] = useState("ChadBelt");
 
   return (
     <>
@@ -14,21 +19,82 @@ const User = () => {
         <NavBar />
       </header>
       <main id="user">
-        <h1>Bem-vindo de volta, {name}.</h1>
+        <h1>Bem-vindo de volta, {fname}.</h1>
         <div className="user-panel">
-          <div className="user-photo-panel">
+          <div className="user-sidebar-panel">
             <img src={logo} />
-            <span className="name badge">{email}</span>
-            <ul>
+            <span className="fname badge">{email}</span>
+            <ul className="details-sheet">
               <li>
-                <b>Username:</b> {name}
+                <div className="detail-cell">
+                  <label>Nome Completo</label>
+                </div>
+                <div className="detail-cell">
+                  <span>
+                    {fname} {lname}
+                  </span>
+                </div>
               </li>
               <li>
-                <b>Email:</b> {email}
+                <div className="detail-cell">
+                  <label>Username</label>
+                </div>
+                <div className="detail-cell">
+                  <span>{fname}</span>
+                </div>
+              </li>
+              <li>
+                <div className="detail-cell">
+                  <label>Email</label>
+                </div>
+                <div className="detail-cell">
+                  <span>
+                    <a href={`mailto:${email}`}>{email}</a>
+                  </span>
+                </div>
+              </li>
+              <li>
+                <div className="detail-cell">
+                  <label>LinkedIn</label>
+                </div>
+                <div className="detail-cell">
+                  <span>
+                    <a href={`https://br.linkedin.com/${linkedin}`}>
+                      {linkedin}
+                    </a>
+                  </span>
+                </div>
+              </li>
+              <li>
+                <div className="detail-cell">
+                  <label>Github</label>
+                </div>
+                <div className="detail-cell">
+                  <span>
+                    <a href={`https://github.com/${github}`}>
+                      github.com/{github}
+                    </a>
+                  </span>
+                </div>
               </li>
             </ul>
           </div>
-          <div className="user-main-content">All</div>
+          <div className="user-main-content">
+            <div className="name-callout">
+              <div className="first-row">
+                <h3 className="username">{username}</h3>
+                <sub className="github-user">
+                  <a href={`https://github.com/${github}`}>@{github}</a>
+                </sub>
+              </div>
+              <div className="second-row fullname">
+                <span>{fname}</span>
+                <span>{mname}</span>
+                <span>{lname}</span>
+              </div>
+            </div>
+            
+          </div>
         </div>
       </main>
     </>
